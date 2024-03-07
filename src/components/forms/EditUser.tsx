cf44ad1153c05a6ab08cab4d24363ab5eee82a12
Tricia Swift <tricia.swift8@gmail.com>
2024-03-06 21:58:29 -0600

    added styling

--- src/components/forms/EditUser.tsx
+++ src/components/forms/EditUser.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { User } from "../../types/User";
 import { useEffect, useState } from "react";
 import { editUser, getUserById } from "../../services/userService";
@@ -6,7 +6,6 @@ import { UserForm } from "./UserForm";
 
 export const EditUser = () => {
   const { userId } = useParams<{ userId: string }>();
-  const navigate = useNavigate();
 
   const [user, setUser] = useState<User>({} as User);
   const [loading, setLoading] = useState(false);
@@ -43,7 +42,7 @@ export const EditUser = () => {
       try {
         setLoading(true);
         await editUser(user);
-        setSuccessMessage("User updated successfully!"); // Set the success message
+        setSuccessMessage("User's information was updated successfully!"); // Set the success message
       } catch (error) {
         console.error("Failed to save user:", error);
         setError("Failed to save user. Please try again later.");
@@ -56,17 +55,45 @@ export const EditUser = () => {
   };
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="loading-message text-lg font-semibold">Loading...</p>
+      </div>
+    );
   }
 
-  if (error) return <p className="error-message text-red-500">{error}</p>;
+  if (error)
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="error-message text-red-500 text-lg font-semibold">
+          {error}
+        </p>
+        <Link
+          to={`/`}
+          className="mt-4 text-blue-500 hover:text-blue-700 underline"
+        >
+          Return to list of users
+        </Link>
+      </div>
+    );
 
   if (successMessage) {
     // If there is a success message, render it instead of the form
     return (
-      <section>
-        <div className="success-message">{successMessage}</div>
-        <Link to={`/`}>Return to list of users</Link>
+      <section className="flex flex-col justify-center items-center h-screen">
+        <div
+          className="success-message flex flex-col items-center bg-green-100 border-green-700 border-2 text-green-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <strong className="font-bold">Success!</strong>
+          <span className="block sm:inline">{successMessage}</span>
+        </div>
+        <Link
+          to={`/`}
+          className="mt-4 text-blue-500 hover:text-blue-700 underline"
+        >
+          Return to list of users
+        </Link>
       </section>
     );
   }
